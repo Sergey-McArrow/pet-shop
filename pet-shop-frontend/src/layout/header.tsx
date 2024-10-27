baceac481b30b6e11ec9d6f273e7d5389ec3868b
Sergey-McArrow <sergey.mcarrow@gmail.com>
2024-10-27 21:48:06 +0100

    add layout, routing

---
+++ pet-shop-frontend/src/layout/header.tsx
@@ -0,0 +1,37 @@
+import { Button } from '@/components/ui/button'
+import { Logo } from '@/components/ui/logo'
+import { ModeToggle } from '@/components/ui/toggle-mode'
+import { ShoppingCart } from 'lucide-react'
+import { Link } from 'react-router-dom'
+
+export const Header = () => {
+  return (
+    <header className="flex h-32 items-center justify-between px-10 py-4 shadow-lg">
+      <div className="flex items-center">
+        <Logo />
+      </div>
+      <nav className="flex space-x-8">
+        <Link to="/" className="">
+          Main Page
+        </Link>
+        <Link to="/categories" className="">
+          Categories
+        </Link>
+        <Link to="/all-products" className="">
+          All products
+        </Link>
+        <Link to="/all-sales" className="">
+          All sales
+        </Link>
+      </nav>
+      <div className="flex items-center gap-4">
+        <ModeToggle />
+        <Link to="/cart" className="">
+          <Button variant="outline" size="icon">
+            <ShoppingCart className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+          </Button>
+        </Link>
+      </div>
+    </header>
+  )
+}
---
+++ pet-shop-frontend/src/pages/home.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+type HomeProps = {}
+
+export const Home: FC<HomeProps> = ({}) => {
+  return <>Home</>
+}
